a534a1d0b311a3c277db21b32f247945446ad3a3
Kevin Bradley <kevin.w.bradley@me.com>
2022-08-23 22:11:33 -0400

    added static library build target

---
+++ M3U8KitStatic/M3U8Kits.h
@@ -0,0 +1,13 @@
+//
+//  M3U8Kits.h
+//  M3U8Kits
+//
+//  Created by Kevin Bradley on 8/23/22.
+//  Copyright © 2022 M3U8Kit. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface M3U8Kits : NSObject
+
+@end
---
+++ M3U8KitStatic/M3U8Kits.m
@@ -0,0 +1,13 @@
+//
+//  M3U8Kits.m
+//  M3U8Kits
+//
+//  Created by Kevin Bradley on 8/23/22.
+//  Copyright © 2022 M3U8Kit. All rights reserved.
+//
+
+#import "M3U8Kits.h"
+
+@implementation M3U8Kits
+
+@end
