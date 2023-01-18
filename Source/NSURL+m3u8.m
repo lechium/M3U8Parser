406234139506761ac825fad44bcca959dd928424
Kevin Bradley <kevin.w.bradley@me.com>
2023-01-18 16:27:03 -0500

    the m3u8 url isnt created properly for some reason, add a special category for it

--- Source/NSURL+m3u8.m
+++ Source/NSURL+m3u8.m
@@ -10,6 +10,12 @@
 
 @implementation NSURL (m3u8)
 
+- (NSURL *)m3u_properM3UURL {
+    NSString *base = [[self baseURL] absoluteString]; //ie https://website.net/Company/185/711/2083252803943/1665503927621
+    return [NSURL URLWithString:[base stringByAppendingFormat:@"/%@", [self relativePath]]];
+    //[NSURL URLWithString:[base stringByAppendingPathComponent:[self relativePath]]]; //relativePath ie: layer_3_x15ae724f9359493b9699514a833cd94b/30310_02__055017_2083254851776_mp4_video_640x360_311000_primary_audio_eng_1_x15ae724f9359493b9699514a833cd94b_3.m3u8
+}
+
 - (NSURL *)m3u_realBaseURL {
     NSURL *baseURL = self.baseURL;
     if (!baseURL) {
